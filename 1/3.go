a45ee541702badec2f8ad7a608eafb05e09da9cd
RashElse <123797128+RashElse@users.noreply.github.com>
2024-09-16 22:05:26 +0300

    Create 3.go

---
+++ 1/3.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	integerVar := 42
+	floatVar := 3.14
+	stringVar := "Привет, Go!"
+	boolVar := true
+
+	fmt.Println("Краткое объявление переменных:")
+	fmt.Println("int:", integerVar)
+	fmt.Println("float64:", floatVar)
+	fmt.Println("string:", stringVar)
+	fmt.Println("bool:", boolVar)
+}
