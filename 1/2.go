1428ae8a1c5693661b9cdc43a6316ac431097fa0
RashElse <123797128+RashElse@users.noreply.github.com>
2024-09-16 22:04:59 +0300

    Create 2.go

---
+++ 1/2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var integerVar int = 42
+	var floatVar float64 = 3.14
+	var stringVar string = "Привет, Go!"
+	var boolVar bool = true
+
+	fmt.Println("Переменная типа int:", integerVar)
+	fmt.Println("Переменная типа float64:", floatVar)
+	fmt.Println("Переменная типа string:", stringVar)
+	fmt.Println("Переменная типа bool:", boolVar)
+}
